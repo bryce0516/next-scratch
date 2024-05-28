70ce0d5fa7aa9a9afcb74373fb2e8490a2b89c5b
bryce0516 <jayhspark@gmail.com>
2024-05-28 10:45:42 +0900

    5. Link Component Navigation(Link 컴포넌트 내비게이션)

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,12 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: {
-    absolute: "",
     default: "Next.js Title",
     template: "%s | Next.js",
   },
@@ -20,8 +20,25 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-         <body>
-        <header className="bg-yellow-400 p-2 my-2">Header</header>
+  <body>
+        <header className="bg-yellow-400 p-2 my-2">
+          <nav>
+            <ul className="flex space-x-2">
+              <li>
+                <Link href="/">Home</Link>
+              </li>
+              <li>
+                <Link href="/about">About</Link>
+              </li>
+              <li>
+                <Link href="/profile">Profile</Link>
+              </li>
+              <li>
+                <Link href="/products">Products</Link>
+              </li>
+            </ul>
+          </nav>
+        </header>
         <main className="flex p-4">{children}</main>
         <footer className="bg-cyan-400 p-2 my-2">Footer</footer>
       </body>
